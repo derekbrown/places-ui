2fe3e469bbd398537ff1481f35659cc607db8293
Derek Brown <derek@derek.am>
2016-07-09 02:08:50 -0400

    Fix tests

--- tests/integration/components/x-toasts-test.js
+++ tests/integration/components/x-toasts-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('x-toasts.js', 'Integration | Component | x toasts.js', {
+moduleForComponent('x-toasts', 'Integration | Component | x toasts', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{x-toasts.js}}`);
+  this.render(hbs`{{x-toasts}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#x-toasts.js}}
+    {{#x-toasts}}
       template block text
-    {{/x-toasts.js}}
+    {{/x-toasts}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
